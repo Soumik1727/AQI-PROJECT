a3041b5bdb519c0316f08ac8db3f81f5c0821afe
Soumik Mondal <soumikmondal1727@gmail.com>
2025-07-22 14:57:18 +0530

    ujghgg

--- client/src/components/AllParticles.jsx
+++ client/src/components/AllParticles.jsx
@@ -4,12 +4,12 @@ import { useWeather } from '../wrapper/weatherDataWrap'
 export default function AllParticles() {
     const { weatherData } = useWeather()
     const  particles = [
-        {name:'PM2.5',value:weatherData.pm2_5,unit:'µg/m³',img:'assets/pm25.svg',full_name:'Particulate Matter '},
-        {name:'PM10',value:weatherData.pm10,unit:'µg/m³',img:'assets/pm10.svg',full_name:'Particulate Matter '},
-        {name:'NO2',value:weatherData.no2,unit:'µg/m³',img:'assets/no2.svg',full_name:'Nitrogen Dioxide '},
-        {name:'O3',value:weatherData.o3,unit:'µg/m³',img:'assets/o3.svg',full_name:'Ozone '},
-        {name:'CO2',value:weatherData.co2,unit:'µg/m³',img:'assets/co2.png',full_name:'Carbon Dioxide'},
-        {name:'CO',value:weatherData.co,unit:'µg/m³',img:'assets/co.svg',full_name:'Carbon Monoxide '},
+        {name:'PM2.5',value:weatherData.pm2_5,unit:'µg/m³',img:'/assets/pm25.svg',full_name:'Particulate Matter '},
+        {name:'PM10',value:weatherData.pm10,unit:'µg/m³',img:'/assets/pm10.svg',full_name:'Particulate Matter '},
+        {name:'NO2',value:weatherData.no2,unit:'µg/m³',img:'/assets/no2.svg',full_name:'Nitrogen Dioxide '},
+        {name:'O3',value:weatherData.o3,unit:'µg/m³',img:'/assets/o3.svg',full_name:'Ozone '},
+        {name:'CO2',value:weatherData.co2,unit:'µg/m³',img:'/assets/co2.png',full_name:'Carbon Dioxide'},
+        {name:'CO',value:weatherData.co,unit:'µg/m³',img:'/assets/co.svg',full_name:'Carbon Monoxide '},
         
     ]
     return (
